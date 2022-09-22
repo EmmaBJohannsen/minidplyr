44bbb0f0aa50edc57b2b89dc230d4a0708c68497
EmmaBJohannsen <emjo@clin.au.dk>
2022-09-22 11:51:19 +0200

    fix param for filter2()

--- R/filter2.R
+++ R/filter2.R
@@ -3,13 +3,13 @@
 #' Filter rows of a data frame. 
 #' 
 #' @param data A data frame. 
-#' @param ... A vector of indices. 
+#' @param ind A vector of indices. 
 #'
 #' @return
 #' @export
 #'
 #' @examples
 #' filter2(iris, 1:2)
-filter2 <- function(data, ...) {
-  data[...,]
+filter2 <- function(data, ind) {
+  data[ind,]
 }
